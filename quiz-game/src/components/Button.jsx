8e350721fe4f80dd0064e6a3e4b897587448e462
Michal Stasiak <michal.stasiak@protonmail.com>
2025-02-21 17:55:48 +0100

    fix onClick in summary

--- quiz-game/src/components/Button.jsx
+++ quiz-game/src/components/Button.jsx
@@ -1,5 +1,9 @@
-const Button = ({ label, onClick }) => {
-  return <button onClick={onClick}>{label}</button>;
+const Button = ({ label, onClick, color = "#0000FF" }) => {
+  return (
+    <button style={{ backgroundColor: color }} onClick={onClick}>
+      {label}
+    </button>
+  );
 };
 
 export default Button;
--- quiz-game/src/components/QuizSummary.jsx
+++ quiz-game/src/components/QuizSummary.jsx
@@ -1,4 +1,5 @@
 import { QUESTIONS } from "../assets/quizQuestions";
+import Button from "./Button";
 
 const QuizSummary = ({ questionAnswers }) => {
   const correctAnswers = questionAnswers.filter(
@@ -9,19 +10,31 @@ const QuizSummary = ({ questionAnswers }) => {
     <div>
       <div>
         {correctAnswers >= 8 ? (
-          <h1>Gratulacja, quiz zaliczony</h1>
+          <h1 style={{ color: "#00ff00" }}>Gratulacja, quiz zaliczony</h1>
         ) : (
-          <h1>Niestety, quiz niezaliczony</h1>
+          <h1 style={{ color: "#FF9494" }}>Niestety, quiz niezaliczony</h1>
         )}
       </div>
       <div>
-        <h2></h2>Twój wynik to {correctAnswers}0.00% ({correctAnswers} z 10
-        poprawnych odpowiedzi)
+        <h2>
+          Twój wynik to{" "}
+          <b style={{ color: correctAnswers >= 8 ? "#00ff00" : "#FF9494" }}>
+            {(correctAnswers / QUESTIONS.length) * 100}%
+          </b>{" "}
+          ({correctAnswers} z 10 poprawnych odpowiedzi){" "}
+        </h2>
       </div>
       <div>
         {QUESTIONS.map((question, index) => (
-          <div key={index} style={{ display: "flex", flexDirection: "column" }}>
-            <p>
+          <div
+            key={index}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              textAlign: "left",
+            }}
+          >
+            <p style={{ color: "#87cefa" }}>
               Pytanie {index + 1}: {question.text}
             </p>
             <p
@@ -34,6 +47,11 @@ const QuizSummary = ({ questionAnswers }) => {
             </p>
           </div>
         ))}
+        <Button
+          color="#006400"
+          label={"Powrót do startu"}
+          onClick={() => window.location.reload()}
+        />
       </div>
     </div>
   );
